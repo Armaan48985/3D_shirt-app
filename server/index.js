077daa0f573d0c30f43d9b1f82f4385cafd8b6e1
Armaan48985 <armaanthakur2308@gmail.com>
2023-12-19 22:07:38 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+﻿import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import dalleroute from './routes/dalle.routes.js'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json({limig: "50mb"}))
+
+app.use('/armaan', dalleroute)
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "hello from dale"})
+})
+
+app.listen(8080, () => console.log("you are on port 8080"))
